f66868ceab60d90c7042ee66330d43e57c06d9ae
Steve <steve@kitkat@gmail.com>
2024-05-27 10:42:04 +0700

    Adds component for indicators

--- presentation/src/main/java/me/androidbox/presentation/home/HomeScreen.kt
+++ presentation/src/main/java/me/androidbox/presentation/home/HomeScreen.kt
@@ -2,41 +2,29 @@
 
 package me.androidbox.presentation.home
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import cafe.adriel.voyager.navigator.LocalNavigator
-import cafe.adriel.voyager.navigator.OnBackPressed
-import cafe.adriel.voyager.navigator.currentOrThrow
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
 import kotlinx.coroutines.launch
 import me.androidbox.presentation.home.components.Footer
 import me.androidbox.presentation.home.components.Header
+import me.androidbox.presentation.home.components.Indicators
 import me.androidbox.presentation.ui.theme.BusbyNimbleSurveyTheme
 import timber.log.Timber
 
@@ -100,25 +88,7 @@ fun HomeScreen(
                         contentAlignment = Alignment.BottomStart) {
 
                         Column(modifier = Modifier.fillMaxWidth()) {
-                            Row(
-                                Modifier
-                                    .wrapContentHeight()
-                                    .fillMaxWidth()
-                                    .padding(bottom = 8.dp),
-                                horizontalArrangement = Arrangement.spacedBy(8.dp)
-                            ) {
-                                repeat(pagerState.pageCount) { iteration ->
-                                    val color =
-                                        if (pagerState.currentPage == iteration) Color.White else Color.LightGray
-                                    Box(
-                                        modifier = Modifier
-                                            .padding(2.dp)
-                                            .clip(CircleShape)
-                                            .background(color)
-                                            .size(8.dp)
-                                    )
-                                }
-                            }
+                            Indicators(pagerState)
 
                             Footer(title = "Working from home Check-In",
                                 description = "We would like to know you feel about our work from home",
