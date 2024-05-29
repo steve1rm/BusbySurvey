4ed03b62702f083ace091a1f2a9d8d1eefbb6937
Steve <steve@kitkat@gmail.com>
2024-05-29 15:03:17 +0700

    Adds home dialog box

---
+++ presentation/src/main/java/me/androidbox/presentation/components/HomeDialog.kt
@@ -0,0 +1,90 @@
+package me.androidbox.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import me.androidbox.presentation.ui.theme.BusbyNimbleSurveyTheme
+
+@Composable
+fun HomeDialog(
+    modifier: Modifier = Modifier,
+    title: String,
+    onDismiss: () -> Unit,
+    description: String,
+    primaryButton: @Composable RowScope.() -> Unit,
+    secondaryButton: @Composable (RowScope.() -> Unit)? = null,
+) {
+    Dialog(onDismissRequest = onDismiss,
+        properties = DialogProperties()
+    ) {
+        Column(modifier = modifier
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Text(
+                text = title,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurface)
+
+            Text(text = description,
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurfaceVariant)
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                secondaryButton?.invoke(this)
+                primaryButton()
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewBusbyRunnerDialog() {
+    BusbyNimbleSurveyTheme {
+        HomeDialog(
+            title = "notification",
+            onDismiss = {},
+            description = "Resume or finish your run",
+            primaryButton = {
+                ActionButton(
+                    label = "Resume", showLoading = false ) {
+                }
+            },
+            secondaryButton ={
+                ActionButton(
+                    label = "Finish", showLoading = false ) {
+                }
+            },
+            modifier = Modifier,
+        )
+    }
+}
