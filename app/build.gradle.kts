2fcba67e4df23d5fa9764672e25ff252048fc470
Steve <steve@kitkat@gmail.com>
2024-05-30 14:18:44 +0700

    Updating unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.timber)
 
+    testImplementation(libs.coroutinesTest)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- presentation/src/test/java/me/androidbox/presentation/authentication/login/LoginViewModelTest.kt
+++ presentation/src/test/java/me/androidbox/presentation/authentication/login/LoginViewModelTest.kt
@@ -1,5 +1,8 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package me.androidbox.presentation.authentication.login
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.runTest
@@ -14,8 +17,8 @@ import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.any
 import org.mockito.Mockito.mock
-import org.mockito.Mockito.`when`
 import org.mockito.kotlin.whenever
+import java.lang.Exception
 
 class LoginViewModelTest {
 
@@ -31,11 +34,9 @@ class LoginViewModelTest {
         // Arrange
         val loginViewModel = LoginViewModel(loginUseCase)
 
-        whenever(loginUseCase.execute(
-            LoginRequestModel(email = loginViewModel., password = any()))).thenReturn(APIResponse.OnSuccess(
-            LoginResponseModel(DataModel(AttributesModel(any(), any(), any(), any(), any()), any(), any()))
-        ))
-
+        whenever(loginUseCase.execute(LoginRequestModel(email = loginViewModel.loginState.email.text.toString(), password = loginViewModel.loginState.email.text.toString())))
+            .thenReturn(APIResponse
+                .OnSuccess(LoginResponseModel(DataModel(AttributesModel("", 0, 0, "", ""), "", ""))))
 
         // Act
         loginViewModel.loginAction(LoginAction.OnLoginClicked)
@@ -49,11 +50,9 @@ class LoginViewModelTest {
         // Arrange
         val loginViewModel = LoginViewModel(loginUseCase)
 
-        whenever(loginUseCase.execute(
-            LoginRequestModel(email = loginViewModel., password = any()))).thenReturn(APIResponse.OnFailure(
-            LoginResponseModel())
-        ))
-
+        whenever(loginUseCase.execute(LoginRequestModel(email = loginViewModel.loginState.email.text.toString(), password = loginViewModel.loginState.email.text.toString())))
+            .thenReturn(APIResponse
+                .OnFailure(Exception()))
 
         // Act
         loginViewModel.loginAction(LoginAction.OnLoginClicked)
